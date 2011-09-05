d34ef964e7c5bfda4e7641a0544fd4092222fc43
Jake Trent <trent.jake@gmail.com>
2011-09-05 11:31:50 -0600

    Sort log entries by startDate

--- rolen/media/js/log/Log.js
+++ rolen/media/js/log/Log.js
@@ -1,6 +1,9 @@
 define(["log/Entry", 'order!lib/underscore', 'order!lib/backbone'], function (Entry) {
   return Backbone.Collection.extend({
     model: Entry,
-    url: '/log'
+    url: '/log',
+    comparator: function(entry) {
+      return entry.get('startDate');
+    }
   });
 });
